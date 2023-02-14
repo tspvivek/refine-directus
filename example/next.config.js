649d137b00658693d476e43b4f2c4e4feddb8206
tspvivek <fliptech.india@gmail.com>
2023-02-14 21:57:25 +0530

    Updated to react 18

--- example/next.config.js
+++ example/next.config.js
@@ -1,3 +1,16 @@
-const withPlugins = require("next-compose-plugins");
-
-module.exports = withPlugins([], {});
+module.exports = {
+    experimental: {
+        newNextLinkBehavior: true,
+    },
+    transpilePackages: [
+        "@pankod/refine-antd",
+        "@pankod/refine-inferencer",
+        "antd",
+        "@ant-design/pro-components",
+        "@ant-design/pro-layout",
+        "@ant-design/pro-utils",
+        "@ant-design/pro-provider",
+        "rc-pagination",
+        "rc-picker",
+    ],
+};
--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -5,9 +5,9 @@ import { Refine } from '@pankod/refine-core';
 import { notificationProvider, LoginPage, Layout, ErrorComponent } from '@pankod/refine-antd';
 import { dataProvider } from '@tspvivek/refine-directus';
 import routerProvider from '@pankod/refine-nextjs-router';
-import '@pankod/refine-antd/dist/styles.min.css';
+import "@pankod/refine-antd/dist/reset.css";
 
-import '@styles/global.css';
+import "@styles/global.css";
 
 import authProvider from 'src/authProvider';
 import { directusClient } from 'src/directusClient';
